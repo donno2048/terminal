donno2048/terminal

commit d7525fd232fe0bef052ea765474ced278fa476e3

Implement preventing auto-scroll on new output (#6062)
## Summary of the Pull Request
Updates the Terminal's scroll response to new output. The Terminal will not automatically scroll if...
- a selection is active, or
- the viewport is at the bottom of the scroll history

## References
#2529 - Spec
#3863 - Implementation

## PR Checklist
* [X] Closes #980
* [X] Closes #3863
* [ ] Tests added/passed
* [ ] Requires documentation to be updated

## Detailed Description of the Pull Request / Additional comments
Updates the `_scrollOffset` value properly in TerminalCore when the cursor moves. We calculate a new `_scrollOffset` based on if we are circling the buffer and how far below the mutable bottom is.

We specifically check for if a selection is active and if the viewport is at the bottom, then use that as a condition for deciding if we should update `_scrollOffset` to the new calculated value or 0 (the bottom of the scroll history).

## Validation Steps Performed
Manual testing. Though I should add automated tests.
- [X] new output
- [X] new output when circling
- [X] new output when circling and viewport is at the top

src/cascadia/TerminalCore/Terminal.cpp
@@ -788,9 +788,9 @@ void Terminal::_AdjustCursorPosition(const COORD proposedPosition)
     // If we're about to scroll past the bottom of the buffer, instead cycle the
     // buffer.
     SHORT rowsPushedOffTopOfBuffer = 0;
+    const auto newRows = std::max(0, proposedCursorPosition.Y - bufferSize.Height() + 1);
     if (proposedCursorPosition.Y >= bufferSize.Height())
     {
-        const auto newRows = proposedCursorPosition.Y - bufferSize.Height() + 1;
         for (auto dy = 0; dy < newRows; dy++)
         {
             _buffer->IncrementCircularBuffer();
@@ -804,7 +804,8 @@ void Terminal::_AdjustCursorPosition(const COORD proposedPosition)
 
     // Move the viewport down if the cursor moved below the viewport.
     bool updatedViewport = false;
-    if (proposedCursorPosition.Y > _mutableViewport.BottomInclusive())
+    const auto scrollAmount = std::max(0, proposedCursorPosition.Y - _mutableViewport.BottomInclusive());
+    if (scrollAmount > 0)
     {
         const auto newViewTop = std::max(0, proposedCursorPosition.Y - (_mutableViewport.Height() - 1));
         if (newViewTop != _mutableViewport.Top())
@@ -817,6 +818,13 @@ void Terminal::_AdjustCursorPosition(const COORD proposedPosition)
 
     if (updatedViewport)
     {
+        // scroll if...
+        //   - no selection is active
+        //   - viewport is already at the bottom
+        const bool scrollToOutput = !IsSelectionActive() && _scrollOffset == 0;
+
+        _scrollOffset = scrollToOutput ? 0 : _scrollOffset + scrollAmount + newRows;
+
         _NotifyScrollEvent();
     }
 

commit 04ac0d926889a05fc9360ed0d22006a5d8479686

Fix viewport moving when we've scrolled up and circled the buffer (#7247)
If you scroll up to view the scrollback, then we want the viewport to
"stay in place", as new output comes in (see #6062). This works fine up
until the buffer circles. In this case, the mutable viewport isn't
actually moving, so we never set `updatedViewport` to true. 

This regressed in #6062
Closes #7222

src/cascadia/TerminalCore/Terminal.cpp
@@ -843,8 +843,12 @@ void Terminal::_AdjustCursorPosition(const COORD proposedPosition)
         }
     }
 
-    if (updatedViewport)
+    // If the viewport moved, or we circled the buffer, we might need to update
+    // our _scrollOffset
+    if (updatedViewport || newRows != 0)
     {
+        const auto oldScrollOffset = _scrollOffset;
+
         // scroll if...
         //   - no selection is active
         //   - viewport is already at the bottom
@@ -852,6 +856,18 @@ void Terminal::_AdjustCursorPosition(const COORD proposedPosition)
 
         _scrollOffset = scrollToOutput ? 0 : _scrollOffset + scrollAmount + newRows;
 
+        // Clamp the range to make sure that we don't scroll way off the top of the buffer
+        _scrollOffset = std::clamp(_scrollOffset,
+                                   0,
+                                   _buffer->GetSize().Height() - _mutableViewport.Height());
+
+        // If the new scroll offset is different, then we'll still want to raise a scroll event
+        updatedViewport = updatedViewport || (oldScrollOffset != _scrollOffset);
+    }
+
+    // If the viewport moved, then send a scrolling notification.
+    if (updatedViewport)
+    {
         _NotifyScrollEvent();
     }
 

src/cascadia/UnitTests_TerminalCore/TerminalBufferTests.cpp
@@ -39,6 +39,8 @@ class TerminalCoreUnitTests::TerminalBufferTests final
     TEST_METHOD(TestWrappingCharByChar);
     TEST_METHOD(TestWrappingALongString);
 
+    TEST_METHOD(DontSnapToOutputTest);
+
     TEST_METHOD_SETUP(MethodSetup)
     {
         // STEP 1: Set up the Terminal
@@ -148,4 +150,86 @@ void TerminalBufferTests::TestWrappingALongString()
     VERIFY_IS_FALSE(row1.GetCharRow().WasWrapForced());
 
     TestUtils::VerifyExpectedString(termTb, TestUtils::Test100CharsString, { 0, 0 });
+}
+
+void TerminalBufferTests::DontSnapToOutputTest()
+{
+    auto& termTb = *term->_buffer;
+    auto& termSm = *term->_stateMachine;
+    const auto initialView = term->GetViewport();
+
+    VERIFY_ARE_EQUAL(0, initialView.Top());
+    VERIFY_ARE_EQUAL(TerminalViewHeight, initialView.BottomExclusive());
+    VERIFY_ARE_EQUAL(0, term->_scrollOffset);
+
+    // -1 so that we don't print the last \n
+    for (int i = 0; i < TerminalViewHeight + 8 - 1; i++)
+    {
+        termSm.ProcessString(L"x\n");
+    }
+
+    const auto secondView = term->GetViewport();
+
+    VERIFY_ARE_EQUAL(8, secondView.Top());
+    VERIFY_ARE_EQUAL(TerminalViewHeight + 8, secondView.BottomExclusive());
+    VERIFY_ARE_EQUAL(0, term->_scrollOffset);
+
+    Log::Comment(L"Scroll up one line");
+    term->_scrollOffset = 1;
+
+    const auto thirdView = term->GetViewport();
+    VERIFY_ARE_EQUAL(7, thirdView.Top());
+    VERIFY_ARE_EQUAL(TerminalViewHeight + 7, thirdView.BottomExclusive());
+    VERIFY_ARE_EQUAL(1, term->_scrollOffset);
+
+    Log::Comment(L"Print a few lines, to see that the viewport stays where it was");
+    for (int i = 0; i < 8; i++)
+    {
+        termSm.ProcessString(L"x\n");
+    }
+
+    const auto fourthView = term->GetViewport();
+    VERIFY_ARE_EQUAL(7, fourthView.Top());
+    VERIFY_ARE_EQUAL(TerminalViewHeight + 7, fourthView.BottomExclusive());
+    VERIFY_ARE_EQUAL(1 + 8, term->_scrollOffset);
+
+    Log::Comment(L"Print enough lines to get the buffer just about ready to "
+                 L"circle (on the next newline)");
+    auto viewBottom = term->_mutableViewport.BottomInclusive();
+    do
+    {
+        termSm.ProcessString(L"x\n");
+        viewBottom = term->_mutableViewport.BottomInclusive();
+    } while (viewBottom < termTb.GetSize().BottomInclusive());
+
+    const auto fifthView = term->GetViewport();
+    VERIFY_ARE_EQUAL(7, fifthView.Top());
+    VERIFY_ARE_EQUAL(TerminalViewHeight + 7, fifthView.BottomExclusive());
+    VERIFY_ARE_EQUAL(TerminalHistoryLength - 7, term->_scrollOffset);
+
+    Log::Comment(L"Print 3 more lines, and see that we stick to where the old "
+                 L"rows now are in the buffer (after circling)");
+    for (int i = 0; i < 3; i++)
+    {
+        termSm.ProcessString(L"x\n");
+        Log::Comment(NoThrowString().Format(
+            L"_scrollOffset: %d", term->_scrollOffset));
+    }
+    const auto sixthView = term->GetViewport();
+    VERIFY_ARE_EQUAL(4, sixthView.Top());
+    VERIFY_ARE_EQUAL(TerminalViewHeight + 4, sixthView.BottomExclusive());
+    VERIFY_ARE_EQUAL(TerminalHistoryLength - 4, term->_scrollOffset);
+
+    Log::Comment(L"Print 8 more lines, and see that we're now just stuck at the"
+                 L"top of the buffer");
+    for (int i = 0; i < 8; i++)
+    {
+        termSm.ProcessString(L"x\n");
+        Log::Comment(NoThrowString().Format(
+            L"_scrollOffset: %d", term->_scrollOffset));
+    }
+    const auto seventhView = term->GetViewport();
+    VERIFY_ARE_EQUAL(0, seventhView.Top());
+    VERIFY_ARE_EQUAL(TerminalViewHeight, seventhView.BottomExclusive());
+    VERIFY_ARE_EQUAL(TerminalHistoryLength, term->_scrollOffset);
 }